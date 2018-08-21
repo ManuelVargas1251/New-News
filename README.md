58d3fa015206c702b783258196b035aea554b866
Manuel Vargas <manuelvargas1251@gmail.com>
2018-08-20 22:07:25 -0500

    update readme
    also deleting old log files. Logs now work

--- README.md
+++ README.md
@@ -2,14 +2,20 @@
 Multi-Part Automated Video Processor/Uploader
 
 - [x] Be able to download videos from youtube
-- [ ] Be able to check when a youtube channel uploads a new video and grab URL 
+- [x] Be able to check when a youtube channel uploads a new video and grab URL
+- [x] Schedule to run every hour or so through task scheduler 
 
 
 ```
 npm install
-```
-
-```
 npm start
 ```
 
+## Typical Log
+```
+----Program starting----
+TimeStamp: 2018-08-21T03:00:54.566Z
+Video Title: President Trump Participates in the Salute t...
+Video URL: L2dUs8o0S0s
+Most recent video does not contain Press Briefing
+```
--- app/searching.js
+++ app/searching.js
@@ -1,10 +1,14 @@
-const request = require('request')
-const ytdl = require('ytdl-core')
-const config = require('../config')
-const fs = require('fs')
+// main app start file
 
-// API Parameters
-const apiKey = config.apiKey,
+const
+    // Imports
+    request = require('request'),
+    ytdl = require('ytdl-core'),
+    config = require('../config'),
+    fs = require('fs'),
+
+    // API Parameters
+    apiKey = config.apiKey,
     channelId = config.channelId,
     youtubeApiUrl = "https://www.googleapis.com/youtube/v3/activities",
     options = "maxResults=1&part=snippet,contentDetails",
@@ -12,57 +16,62 @@ const apiKey = config.apiKey,
 
 
 
-// simple truncate function https://stackoverflow.com/a/1199420/7298219
+// String Truncate function; source: https://stackoverflow.com/a/1199420/7298219
 String.prototype.trunc = String.prototype.trunc ||
     function (n) {
         return (this.length > n) ? this.substr(0, n - 1) + '...' : this;
     };
 
-
-
+// Request the most recent video from the white house
 function searching() {
-
-    // request most recent video from the white house
     request(
         youtubeapistring,
         { json: true },
         (err, res, body, ) => {
             if (err) { return console.log(err); }
-
-            let videoResponse = body.items[0],
-                videoTitle = videoResponse.snippet.title,                   //video title
-                videoURL = videoResponse.contentDetails.upload.videoId,     //video URL
-                currentDate = new Date().toISOString()
-                // stream = fs.createWriteStream('file://log.json',
-                //     { flags: 'a' },
-
-                // );
-
-            //stream.write(currentDate+videoTitle );
-
-            // Print latest video's information
-            console.log(
-                "TimeStamp: " + currentDate + '\n' +
-                "Video Title: " + videoTitle.trunc(50) + '\n' +
-                "Video URL: " + videoURL
-            )
-
-           // fs.appendFileSync('../videoLog.json', 'data to append');
-
-            // search the title for Press Briefing
-            // if found, download video
-            // else, log title and date
-            if (videoTitle.includes('Press Briefing')) {
-
-                //download video
-                ytdl('https://www.youtube.com/watch?v=' + videoURL)     // Source Url
-                    .pipe(fs.createWriteStream('./downloads/' + videoTitle + '.mp4'));  // Destination Location
-            }
-            else (
-                console.warn('Most recent video does not contain Press Briefing')
-            )
+            responseHandler(body)   // handle this in a function below
         }
     )
 }
 
 searching()
+
+
+function responseHandler(body) {
+    let videoResponse = body.items[0],
+        videoTitle = videoResponse.snippet.title,                   //video title
+        videoURL = videoResponse.contentDetails.upload.videoId,     //video URL
+        currentDate = new Date().toISOString()
+    stream = fs.createWriteStream('log.log',
+        { flags: 'a' },
+        (err) => {
+            throw err;
+        }
+    );
+
+    // log to file
+    stream.write(currentDate + '  ' + videoURL + '  ' + videoTitle + '\n');
+
+    // log to console
+    console.log(
+        "TimeStamp: " + currentDate + '\n' +
+        "Video Title: " + videoTitle.trunc(45) + '\n' +
+        "Video URL: " + videoURL
+    )
+
+    //if()
+
+    // search the title for Press Briefing
+    // if found, download video
+    // else, log title and date
+    if (videoTitle.includes('Press Briefing')) {
+
+        //download video
+        ytdl('https://www.youtube.com/watch?v=' + videoURL)     // Source Url
+            .pipe(fs.createWriteStream('./downloads/' + videoTitle + '.mp4'));  // Destination Location
+    }
+    else (
+        console.warn('Most recent video does not contain Press Briefing')
+        // send me a text and wait for a response from me??
+    )
+}
